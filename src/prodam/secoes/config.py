5e074b86d14bb0b16b5de3352fb7ec40990be585
dadlo <dadlobr@gmail.com>
2015-06-25 11:49:57 -0300

    Remove parte não utilizada

--- src/prodam/secoes/config.py
+++ src/prodam/secoes/config.py
@@ -1,15 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from Products.CMFPlone.interfaces import INonInstallable
-from zope.interface import implements
-
 PROJECTNAME = 'prodam.secoes'
-
-
-class HiddenProfiles(object):
-    implements(INonInstallable)
-
-    def getNonInstallableProfiles(self):
-        return [
-            u'prodam.secoes:uninstall',
-        ]
