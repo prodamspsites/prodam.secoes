687ee4641467c64f2b0896443ba392ced7faaad0
mosesjr <shank.sa@gmail.com>
2015-09-17 16:31:46 -0300

    Corrige pipeline

--- src/prodam/secoes/blueprint.py
+++ src/prodam/secoes/blueprint.py
@@ -1,6 +1,5 @@
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
-from zope.component import provideUtility
 from zope.interface import classProvides
 from zope.interface import implements
 import pprint
@@ -9,12 +8,12 @@ import pprint
 class Fields(object):
     classProvides(ISectionBlueprint)
     implements(ISection)
+    import pdb
+    pdb.set_trace()
 
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.pprint = pprint.PrettyPrinter().pprint
-
-provideUtility(Fields, name=u'collective.transmogrifier.tests.exampleconstructor')
 
 
 class Create(object):
@@ -24,11 +23,3 @@ class Create(object):
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.pprint = pprint.PrettyPrinter().pprint
-
-    def __iter__(self):
-        for item in self.previous:
-            self.pprint(sorted(item.items()))
-            yield item
-
-
-provideUtility(Create, name=u'collective.transmogrifier.tests.exampleconstructor')
